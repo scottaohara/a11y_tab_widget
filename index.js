f045eb177f3aa9fd454ace6705d7280474e07015
Scott O'Hara <scottaohara@gmail.com>
2018-09-28 14:26:26 -0400

    object.assign ie11 polyfill

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 'use strict';
 
+if (typeof Object.assign != 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
+
 // add utilities
 var util = {
   keyCodes: {
@@ -240,7 +270,7 @@ var util = {
     var onKeyPress = function ( e ) {
       var keyCode = e.keyCode || e.which;
 
-      switch (keyCode) {
+      switch ( keyCode ) {
         case util.keyCodes.SPACE:
           e.preventDefault();
           activateTab();
